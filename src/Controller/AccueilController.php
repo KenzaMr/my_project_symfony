7bedc73f899672f19144d1318f6ea56a0f10a50b
unknown <kenza.mroudjae@outlook.com>
2024-08-30 14:04:10 +0200

    ajout 1er page

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -6,13 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/accueil', name: 'app_accueil_')]
 class AccueilController extends AbstractController
 {
-    #[Route('/accueil', name: 'app_accueil')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
-        ]);
+        return $this->render('front/accueil/index.html.twig', []);
     }
 }
